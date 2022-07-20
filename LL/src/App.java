c6cf4c9f66b3790f51be4169cc6d5cf9dc9b427d
MayurSMahajan <mayur23smahajan@gmail.com>
2022-07-20 17:44:42 +0530

    Merge Two Sorted Lists LeetCode#21

--- LL/src/App.java
+++ LL/src/App.java
@@ -2,18 +2,23 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        LL list = new LL();
-        // list.insertAtFirst(1);
-        // list.insertAtFirst(1);
-        // list.insertAtFirst(2);
-        // list.insertAtFirst(2);
-        // list.insertAtFirst(2);
-        // list.insertAtFirst(3);
-        // list.insertAtFirst(4);
+        LL list1 = new LL();
+        list1.insertAtFirst(4);
+        list1.insertAtFirst(2);
+        list1.insertAtFirst(1);
+    
+        LL list2 = new LL();
+        list2.insertAtFirst(4);
+        list2.insertAtFirst(3);
+        list2.insertAtFirst(1);
+
+        list1.display();
+        list2.display();
+
+        LL list3 = new LL();
+        list3.mergeTwoSortedLists(list1.getHead(), list2.getHead());
+        list3.display();
         
-        list.display();
-        list.removeDuplicates();
-        list.display();
     }
 }
 
--- LL/src/LL.java
+++ LL/src/LL.java
@@ -1,12 +1,15 @@
 public class LL {
     
     private Node head;
-    
 
     public void insertAtFirst(int value){
         Node node = new Node(value);
         node.next = head;
         head = node;
+    }
+
+    public Node getHead(){
+        return head;
     }
 
     public void display(){
@@ -24,6 +27,7 @@ public class LL {
         System.out.println(temp.val + " -> END");
     }
 
+    //Solution of LeetCode problem number 83
     public void removeDuplicates(){
         if(head == null){
             return;
@@ -38,6 +42,62 @@ public class LL {
                 temp = temp.next;
             }
         }
+    }
+
+    //Cleaner code since I am able to separate my code from the actual leetcode solution.
+    public void mergeTwoSortedLists(Node l1, Node l2){
+        head = mergeTwoSorted(l1, l2);
+    }
+
+    //Solultion for Merge Two Sorted Lists in LeetCode problem no 21
+    public Node mergeTwoSorted(Node l1, Node l2){
+        if (l1 == null && l2 == null) {
+            return l1;
+        }
+        if (l1 != null && l2 == null) {
+            return l1;
+            
+        }
+        if (l2 != null && l1 == null){
+            return l2;
+        }
+        
+        Node l3 = null;
+        Node t3;
+        Node fl3 = null;
+
+        while (l1 != null && l2 != null) {
+            if(l1.val <= l2.val){
+                t3 = l1;
+                l1 = l1.next;
+            }
+            else{
+                t3 = l2;
+                l2 = l2.next;
+            }
+
+            if (l3 != null) {
+                l3.next = t3;
+                l3 = l3.next;
+            }
+            else{
+                fl3 = t3;
+                l3 = t3;
+            }
+        }
+
+        if(l1 != null && l2 == null){
+            l3.next = l1;
+            return fl3;
+        }
+        if(l2 != null && l1 == null){
+            l3.next = l2;
+            return fl3;
+        }
+        
+        return fl3;
+        
+        
     }
 
     private class Node {

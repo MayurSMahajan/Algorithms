df6f4d06535083072655cc9c28331040e9862ef3
Mayur Mahajan <47064215+MayurSMahajan@users.noreply.github.com>
2022-08-12 20:28:49 +0530

    Longest Common Prefix LeetCode#14
    Find the problem here : https://leetcode.com/problems/longest-common-prefix/

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,33 @@
+import java.lang.Math;
+import java.util.*;
+
+class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] arr = {"car","cart","ca"};
+        System.out.println(longestCommonPrefix(arr));
+        
+    }
+    
+    public static String longestCommonPrefix(String[] strs) {
+        String coms = strs[0];
+        String ans = "";
+        
+        for(int i=1;i<strs.length;i++){
+            int min = Math.min(coms.length(),strs[i].length());
+            for(int j=0;j<min;j++){
+                if(coms.charAt(j) != strs[i].charAt(j)){
+                    break;
+                }
+                else{
+                    ans += coms.charAt(j);
+                }
+                
+            }
+            coms = ans;
+            ans = "";
+            
+        }
+        return coms;
+    }
+    
+}

17d3a7fa3045228e11fd07d78c92f9685a1124f2
MayurSMahajan <mayur23smahajan@gmail.com>
2022-08-30 22:15:56 +0530

    LeetCode#448 FindDisappeared

--- CyclicSort.java
+++ CyclicSort.java
@@ -1,34 +1,73 @@
+import java.util.*;
+
 /**
  * CyclicSort
  */
 public class CyclicSort {
     public static void main(String[] args) {
         BasicCyclicSort bsc = new BasicCyclicSort();
-        int[] arr = { 3, 4 , 5, 1, 2 };
-        bsc.sort(arr);
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        // int[] nums = { 3, 4, 5, 1, 2 };
+        // System.out.println(Arrays.toString(bsc.sort(arr)));
+        List<Integer> list = bsc.findDisappearedNumbers(arr);
+        for (Integer element : list) {
+            System.out.println(element);
+        }
+
     }
 }
 
 /**
- * BasicCyclicSort
+ * BasicCyclicSort - Only works for a continous range of 1 to N elements of an
+ * array.
  */
 class BasicCyclicSort {
-    void sort(int[] arr) {
+    int[] sort(int[] nums) {
 
         int i = 0;
-        while(i < arr.length) {
-            while (arr[i] - 1 != i) {
-                int temp = arr[arr[i] - 1];
-                arr[arr[i] - 1] = arr[i];
-                arr[i] = temp;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
             }
-            i++;
+
+        }
+        return nums;
+    }
+
+    void swap(int[] nums, int i, int correct) {
+        int temp = nums[i];
+        nums[i] = nums[correct];
+        nums[correct] = temp;
+        
+    }
+
+    //Problem : https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+    List<Integer> findDisappearedNumbers(int[] nums) {
+
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+
         }
 
-        for (int j : arr) {
-            System.out.println(j);
+        List<Integer> intList = new ArrayList<Integer>(nums.length);
+        for (int index = 0; index < nums.length; index++) {
+            if(index + 1 != nums[index]){
+                intList.add(index + 1 );
+            }
         }
 
+        return intList;
     }
 
 }

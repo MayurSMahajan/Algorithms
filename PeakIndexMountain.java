7a4cb0262b84641beef3dc406d54fe36d7cbcbd2
MayurSMahajan <mayur23smahajan@gmail.com>
2022-09-10 15:47:59 +0530

    FindPeakElement #162

--- PeakIndexMountain.java
+++ PeakIndexMountain.java
@@ -1,16 +1,15 @@
-//Leetcode 852
-//https://leetcode.com/problems/peak-index-in-a-mountain-array/
-
-
 public class PeakIndexMountain {
     public static void main(String[] args) {
-        PIM pim = new PIM();
+        PIMII pim = new PIMII();
         int[] arr = {3,5,3,2,0};
         System.out.println(pim.peakIndexInMountainArray(arr));
     }
 }
 
-class PIM {
+//Leetcode 852
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class PIMII {
     public int peakIndexInMountainArray(int[] arr) {
         int ans = 0;
 
@@ -45,4 +44,72 @@ class PIM {
 
         return ans;
     }    
+}
+
+//LeetCode 162
+//https://leetcode.com/problems/find-peak-element/
+
+class PIMI {
+    public int findPeakElement(int[] arr) {
+        int ans = 0;
+        
+        if(arr.length == 1){
+            return 0;
+        }
+        if(arr.length == 2){
+            return arr[0] > arr[1] ? 0 : 1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int prev;
+            int next;
+
+            //handling special edge case.
+            if(mid == 0){
+                if(arr[mid] > arr[mid+1]){
+                    return 0;
+                }
+                else{
+                    prev = Integer.MIN_VALUE;
+                }
+            }
+            else{
+                prev = arr[mid-1];
+            }
+            
+            if(mid == arr.length - 1){
+                if(arr[mid] > arr[mid-1]){
+                    return mid;
+                }
+                else{
+                    next = Integer.MIN_VALUE;
+                }
+            }
+            else{
+                next = arr[mid+1];
+            }
+            
+            
+            boolean isMidGreaterThanPrev = arr[mid] > prev;
+            boolean isMidGreaterThanNext = arr[mid] > next;
+
+            if(isMidGreaterThanNext && isMidGreaterThanPrev){
+                ans = mid;
+                break;
+            }
+            else if(isMidGreaterThanPrev){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+    
 }

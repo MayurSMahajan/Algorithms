fccbf27f573287ed7639877d358c1c1a329a34a1
MayurSMahajan <mayur23smahajan@gmail.com>
2022-10-22 00:33:59 +0530

    FibonacciMemoization #509

--- FibMemoization.java
+++ FibMemoization.java
@@ -4,9 +4,12 @@ import java.util.*;
 public class FibMemoization{
     
     public static void main(String[] args) {
-        FibMemoization cs = new FibMemoization();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        System.out.println(cs.climbStairs(8,map));
+        // FibMemoization cs = new FibMemoization();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // System.out.println(cs.climbStairs(8,map));
+
+        Fibonacci fb = new Fibonacci();
+        System.out.println(fb.fib(30));
     }
     
     public int climbStairs(int n, HashMap<Integer, Integer> map) {
@@ -21,5 +24,25 @@ public class FibMemoization{
         ans = climbStairs(n-1,map) + climbStairs(n-2,map);
         map.put(n,ans);
         return ans;
+    }
+    
+}
+
+
+// https://leetcode.com/problems/fibonacci-number/
+// 100% faster and uses 78% less space.
+class Fibonacci{
+    public int fib(int n) {
+        HashMap<Integer,Integer> memo = new HashMap<Integer,Integer>();
+        return fibonacci(n, memo);
+    }
+    
+    private int fibonacci(int n, HashMap<Integer,Integer> memo){
+        
+        if(n == 1 || n == 0 ) return n;
+        if(memo.get(n) != null) return memo.get(n);
+        
+        memo.put(n, (fibonacci(n-1,memo)+fibonacci(n-2,memo)));
+        return memo.get(n);
     }
 }

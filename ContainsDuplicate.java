d77048abed4fe4ee7af19af971f71a90b9e07a4c
MayurSMahajan <mayur23smahajan@gmail.com>
2022-10-21 12:40:36 +0530

    ContainsDupliUsingSet #217

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * ContainsDuplicate -- Problem Series LeetCode
@@ -8,6 +9,7 @@ public class ContainsDuplicate {
         int[] nums = {1,1,1,3,3,4,3,2,4,2};
         CD cd = new CD();
         System.out.println(cd.containsDuplicateI(nums));
+        System.out.println(cd.containsDuplicateUsingSet(nums));
     }
 }
 
@@ -15,6 +17,7 @@ class CD{
     
     //https://leetcode.com/problems/contains-duplicate/
     
+    //Time Complexity : O(n logn) bcz sort()'s complexity is O(n log n)
     public boolean containsDuplicateI(int[] nums) {
         if(nums.length == 1) return false;
         
@@ -30,6 +33,22 @@ class CD{
             j++;
         }
         
+        return false;
+    }
+
+    //This is more optimized. A hashset can't contain duplicate values
+    //We check if some value exists in the hashset, if yes then we have duplicates
+    //if no then there are no duplicates and hence we add that element in the set
+    //Time Complexity : O(n)
+    public boolean containsDuplicateUsingSet(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
         return false;
     }
 }

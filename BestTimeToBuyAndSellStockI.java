747662628a4329d21767aa54fe84c5f214ebb4a3
Mayur Mahajan <47064215+MayurSMahajan@users.noreply.github.com>
2023-02-25 08:10:06 +0530

    Optimize Best Time to Buy and Sell #121

--- BestTimeToBuyAndSellStockI.java
+++ BestTimeToBuyAndSellStockI.java
@@ -28,3 +28,23 @@ class BTBSSI {
         return max_profit;
     }
 }
+
+//Optimized: Check if the current price is the lowest we have seen, in that don't compare max_profit at that stage.
+//  If not, check if by subtracting the current price from the lowest ever price we can find the greatest profit ever.
+// Note finding the lowest price and calculating max profit are mutually exclusive.
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lowest_price = prices[0], max_profit= 0;
+        for(int i=1; i< prices.length; i++){
+            if(prices[i] < lowest_price){
+                lowest_price = prices[i];
+                continue;
+            }
+            max_profit = Math.max(max_profit, (prices[i] - lowest_price));
+        }
+        return max_profit;
+    }   
+}
+
+*/
